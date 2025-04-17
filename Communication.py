7aadb7a4eb550f2a95e1d9b8c3079171d8499084
Haaplix 1.5 <Adamalihussain1306@gmail.com>
2025-04-17 10:54:28 +0200

    add ping_pong

--- Communication.py
+++ Communication.py
@@ -6,42 +6,65 @@ import json
 class Communication:
 
     def __init__(self,ip,port):
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__s = s
-        self.ip = ip
-        self.port = port
+        sb = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #? socket subscribe
+        sg = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #? socket for the "game" 
+        self.__sb = sb
+        self.__sg = sg
+        self.ip_gest = ip     #! IP et port gestionnaire
+        self.port_gest = port
+        self.port = 8080
         self.subscribe()
 
 
     def subscribe(self):
-        self.__s.connect((self.ip, self.port))
+        self.__sb.connect((self.ip_gest, self.port_gest))
 
         data = json.dumps({"request": "subscribe", 
-                "port": 8080,
+                "port": self.port,
                 "name": "tkt",
                 "matricules" : ["23022", "23061"]})
 
-        send = self.__s.send(data.encode())
+        send = self.__sb.send(data.encode())
         
         chunk = []
         finished = False
 
         while not finished:
 
-            data = self.__s.recv(1024)
+            data = self.__sb.recv(1024)
             chunk.append(data)
             finished = data == b''
         response = json.loads(b"".join(chunk).decode())
 
         if response.get("response") == "ok":
             print("Subscription successful")
+            self.__sb.close()
+            self.__sg.bind((socket.gethostname(), self.port))
+            self.ping_pong()
+            
         else:
             print("Subscription failed")
             self.subscribe()
 
-
+    def ping_pong(self):
         
-Communication("172.17.10.43",3000) 
+        self.__sg.listen()       #* on va surement mettre listen et if dans run comme ça, ça doit pas passer H24 par ping_pong donc en gros y'aura que le dumps et send
+        gest, addr =self.__sg.accept()
+
+        respond = json.loads(gest.recv(1024).decode())
+
+        if respond.get("request") == "ping":
+            print("Ping received")
+            data = json.dumps({"response": "pong"})
+            gest.send(data.encode())
+
+
+
+
+
+
+
+Communication("172.17.10.43",3000) #! IP Julie
 
 
 
