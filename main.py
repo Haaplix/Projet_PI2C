b027abb5ad2c69326e95a71976b7cd606cfb57be
JuTerrr <julie@terryn.org>
2025-04-22 10:49:19 +0200

    Try random bot

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 
 class game:
     def __init__(self):
-        self.list_piece_possible = ["BDEC","BDEP","BDFC","BDFP","BLEC","BLFC","BLEP","BLFP",
-                      "SDEC","SDEP","SDFC","SDFP","SLEC","SLFC","SLEP","SLFP"]
+        self.list_piece_possible = [set("BDEC"),set("BDEP"),set("BDFC"),set("BDFP"),set("BLEC"),set("BLFC"),set("BLEP"),set("BLFP"),
+                      set("SDEC"),set("SDEP"),set("SDFC"),set("SDFP"),set("SLEC"),set("SLFC"),set("SLEP"),set("SLFP")]
 
     def move(self, state):
 
@@ -24,10 +24,23 @@ class game:
         else: 
             self.piece_got = self.state["piece"]
             self.remove()
+            print(str(random.choice(self.list_piece_possible)).join)
+            return {"response": "move",
+                    "move": {"pos":self.pos(),
+                             "piece":str(random.choice(self.list_piece_possible)).join}
+                    }
     
 
     def remove(self):
-        self.list_piece_possible.remove(self.piece_got)
+        self.list_piece_possible.remove(set(self.piece_got))
         for i in self.board:
             if i != None:
-                self.list_piece_possible.remove(i)
+                self.list_piece_possible.remove(set(i))
+
+    def pos(self):
+        L = []
+        for i,t in enumerate(self.board):
+            if t == None:
+                L.append(i)
+        return random.choice(L)
+
