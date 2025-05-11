b5996016f036500b07d686dc0ea163261724d907
Haaplix <Adamalihussain1306@gmail.com>
2025-05-11 22:34:49 +0200

    add a unit test

--- test_main.py
+++ test_main.py
@@ -2,7 +2,7 @@ import pytest
 import main 
 
 def test_si_dernier_move():
-    assert main.game().move({
+  assert main.game().move({
   "players": ["LUR", "FKY"],
   "current": 0,
   "board": ["BDEC",None,"BDFC","BDFP","BLEC","BLFC","BLEP","BLFP",
@@ -18,3 +18,19 @@ def test_si_dernier_move():
 #   "board": [None,None,None,None,None,None,None,None,None,None,None,None,None,None,None,None],
 #   "piece": "BDEP"
 #         }) == 
+
+
+def test_si_possi_win():
+    result = main.game().move({
+        "players": ["LUR", "FKY"],
+        "current": 0,
+        "board": ["BDEC", None, "BDFC", "BDFP",
+                  "BLEC", None, "BLEP", None,
+                  None, None, "SDFC", "SDFP",
+                  None, None, None, "SLFP"],
+        "piece": "BDEP"
+    })
+    
+    assert result["response"] == "move"
+    assert result["move"]["pos"] == 1
+    assert sorted(result["move"]["piece"]) == sorted("LBFC")
